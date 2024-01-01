c067f32b38d26668618fbea967de43a293c08994
Zeyad Alqutaifan <z.alqutaifan@coderz-tech.com>
2024-01-01 14:33:57 +0300

    Create My Posts

--- app/src/main/java/com/example/mafqodati/adapters/RecyclerMyPostAdapter.java
+++ app/src/main/java/com/example/mafqodati/adapters/RecyclerMyPostAdapter.java
@@ -1,7 +1,5 @@
 package com.example.mafqodati.adapters;
 
-
-
 import static com.example.mafqodati.util.Constants.convertTimeMillsToDateString;
 
 import android.view.LayoutInflater;
@@ -12,6 +10,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -19,31 +18,38 @@ import com.bumptech.glide.load.DecodeFormat;
 import com.bumptech.glide.request.RequestOptions;
 import com.example.mafqodati.R;
 import com.example.mafqodati.models.Post;
-import com.example.mafqodati.models.User;
-import com.example.mafqodati.util.FireStore;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.chip.Chip;
-import com.google.firebase.firestore.DocumentSnapshot;
 
-public class RecyclerPostAdapter extends FirestoreRecyclerAdapter<Post, RecyclerPostAdapter.ViewHolder> {
+public class RecyclerMyPostAdapter extends FirestoreRecyclerAdapter<Post, RecyclerMyPostAdapter.ViewHolder> {
 
-    public RecyclerPostAdapter(@NonNull FirestoreRecyclerOptions<Post> options) {
-        super(options);
+    public interface OnItemClickListener {
+        void onItemClick(String postId);
     }
 
-    @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_post_card, parent, false);
-        return new ViewHolder(view);
+    public interface OnDeleteClickListener {
+        void onDeleteClick(String postId);
+    }
+
+    private OnItemClickListener onItemClickListener;
+    private OnDeleteClickListener onDeleteClickListener;
+
+    /**
+     * Create a new RecyclerView adapter that listens to a Firestore Query.
+     *
+     * @param options
+     */
+
+    public RecyclerMyPostAdapter(@NonNull FirestoreRecyclerOptions<Post> options, OnItemClickListener onItemClickListener, OnDeleteClickListener onDeleteClickListener) {
+        super(options);
+        this.onItemClickListener = onItemClickListener;
+        this.onDeleteClickListener = onDeleteClickListener;
+
     }
 
     @Override
     protected void onBindViewHolder(@NonNull ViewHolder holder, int position, @NonNull Post model) {
-
         holder.txtPostTitle.setText(model.getTitle());
         holder.txtPostContent.setText(model.getDescription());
         holder.txtCreationDate.setText(convertTimeMillsToDateString(model.getCreationDate()));
@@ -57,22 +63,20 @@ public class RecyclerPostAdapter extends FirestoreRecyclerAdapter<Post, Recycler
                     .apply(RequestOptions.formatOf(DecodeFormat.PREFER_ARGB_8888))
                     .into(holder.imgPostImage);
         }
-        FireStore.getUserData(model.getWriterId()).addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()){
-                    DocumentSnapshot documentSnapshot = task.getResult();
-                    if (documentSnapshot.exists()) {
-                        User user = documentSnapshot.toObject(User.class);
-                        holder.btnCall.setText(user.getUserPhone());
-                    }
-                }
-            }
-        });
+
 
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_my_post_card, parent, false);
+        return new ViewHolder(view);
+
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        CardView container;
         TextView txtPostTitle;
         TextView txtPostContent;
 
@@ -83,7 +87,7 @@ public class RecyclerPostAdapter extends FirestoreRecyclerAdapter<Post, Recycler
 
         TextView txtImgCount;
         ImageView imgPostImage;
-        Button btnCall;
+        Button btnDelete;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -91,11 +95,32 @@ public class RecyclerPostAdapter extends FirestoreRecyclerAdapter<Post, Recycler
             txtPostContent = itemView.findViewById(R.id.txtPostContent);
             txtCreationDate = itemView.findViewById(R.id.txtCreationDate);
             chpType = itemView.findViewById(R.id.chpType);
+            container = itemView.findViewById(R.id.container);
             chpCity = itemView.findViewById(R.id.chpCity);
             chpCategory = itemView.findViewById(R.id.chpCategory);
             imgPostImage = itemView.findViewById(R.id.imgPostMain);
             txtImgCount = itemView.findViewById(R.id.txtImgCount);
-            btnCall = itemView.findViewById(R.id.btnCall);
+            btnDelete = itemView.findViewById(R.id.btnDelete);
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Call onItemClick when an item is clicked
+                    if (onItemClickListener != null) {
+                        onItemClickListener.onItemClick(getSnapshots().getSnapshot(getAdapterPosition()).getId());
+                    }
+                }
+            });
+
+            btnDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (onDeleteClickListener != null) {
+                        onDeleteClickListener.onDeleteClick(getSnapshots().getSnapshot(getAdapterPosition()).getId());
+                    }
+                }
+            });
+
+
         }
     }
 }
--- app/src/main/java/com/example/mafqodati/fragments/MyPostFragment.java
+++ app/src/main/java/com/example/mafqodati/fragments/MyPostFragment.java
@@ -1,53 +1,94 @@
 package com.example.mafqodati.fragments;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.mafqodati.R;
+import com.example.mafqodati.adapters.RecyclerMyPostAdapter;
 import com.example.mafqodati.adapters.RecyclerPostAdapter;
+import com.example.mafqodati.databinding.FragmentMyPostBinding;
 import com.example.mafqodati.models.Post;
+import com.example.mafqodati.util.FireStore;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class MyPostFragment extends Fragment {
-    private RecyclerView recyclerMyPost;
-    private RecyclerPostAdapter recyclerPostAdapter;
+    FragmentMyPostBinding binding;
+    private RecyclerMyPostAdapter recyclerMyPostAdapter;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_my_post, container, false);
-        recyclerMyPost = view.findViewById(R.id.recyclerMyPost);
-        FirestoreRecyclerOptions<Post> postOptions = new FirestoreRecyclerOptions.Builder<Post>()
-                .setQuery(db.collection("posts").whereEqualTo("writerId",mAuth.getCurrentUser().getUid() ), Post.class)
-                .build();
-        recyclerPostAdapter = new RecyclerPostAdapter(postOptions);
-        recyclerMyPost.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerMyPost.setAdapter(recyclerPostAdapter);
+        binding = FragmentMyPostBinding.inflate(inflater, container, false);
 
-        return view;
+        fillMyPostRecycler();
+
+
+        return binding.getRoot();
+    }
+
+
+    RecyclerMyPostAdapter.OnDeleteClickListener onDeleteClickListener = postId -> {
+        Log.v("LOG_ON_ITEM" , postId);
+        new MaterialAlertDialogBuilder(getActivity())
+                .setTitle("Deleted Item")
+                .setCancelable(false)
+                .setPositiveButton("Yes, Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        FireStore.postRef(postId).delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                fillMyPostRecycler();
+                            }
+                        });
+                    }
+                })
+                .setNegativeButton("No" , null)
+                .show();
+    };
+
+    RecyclerMyPostAdapter.OnItemClickListener onItemClickListener = postId -> {
+        Log.v("LOG_ON_ITEM" , postId);
+    };
+
+    private void fillMyPostRecycler() {
+        FirestoreRecyclerOptions<Post> postOptions = new FirestoreRecyclerOptions.Builder<Post>()
+                .setQuery(db.collection("posts").whereEqualTo("writerId", mAuth.getCurrentUser().getUid()), Post.class)
+                .build();
+        recyclerMyPostAdapter = new RecyclerMyPostAdapter(postOptions, onItemClickListener , onDeleteClickListener);
+        binding.recyclerMyPost.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.recyclerMyPost.setAdapter(recyclerMyPostAdapter);
+        recyclerMyPostAdapter.startListening();
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        recyclerPostAdapter.startListening();
+        recyclerMyPostAdapter.startListening();
     }
+
     @Override
     public void onStop() {
         super.onStop();
-        recyclerPostAdapter.stopListening();
+        recyclerMyPostAdapter.stopListening();
     }
 
 }
